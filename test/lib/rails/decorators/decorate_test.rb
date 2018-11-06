cc4570b23de161d266449f455ac10e2b308f10b7
Ben Crouse <bcrouse@weblinc.com>
2018-11-06 10:17:11 -0500

    Fix decorating namespaced classes with same name
    This is accomplished by using the full module path to generate the
    module name for decoration.
    
    Fixes #7

--- test/lib/rails/decorators/decorate_test.rb
+++ test/lib/rails/decorators/decorate_test.rb
@@ -11,41 +11,59 @@ class DecorateTest < Minitest::Test
     end
   end
 
+  module Namespace
+    class TestClass < TestClass
+      def foo
+        'baz'
+      end
+    end
+  end
+
   class ChildClass < TestClass
   end
 
-  def test_decorate
-    decorate(TestClass, with: 'testing') do
-      class_methods do
-        def foo
-          "#{super}|baz"
-        end
-      end
-
-      decorated { attr_reader :test }
-
+  decorate(TestClass, with: 'testing') do
+    class_methods do
       def foo
         "#{super}|baz"
       end
     end
 
+    decorated { attr_reader :test }
+
+    def foo
+      "#{super}|baz"
+    end
+  end
+
+  decorate(ChildClass, with: 'testing') do
+    def baz
+      'decorated'
+    end
+  end
+
+  decorate(Namespace::TestClass, with: 'testing') do
+    def foo
+      'namespace decorated'
+    end
+  end
+
+  def test_decorate
     assert(TestClass.new.respond_to?(:test))
     assert_equal('bar|baz', TestClass.new.foo)
     assert_equal('bar|baz', TestClass.foo)
   end
 
   def test_subclass_decoration
-    decorate(ChildClass, with: 'testing') do
-      def baz
-        'decorated'
-      end
-    end
-
     assert_equal('decorated', ChildClass.new.baz)
   end
 
+  def test_decorating_within_a_namespace
+    assert_equal('namespace decorated', Namespace::TestClass.new.foo)
+  end
+
   def test_module_definition
-    decorate(TestClass, with: 'tests') {}
-    assert(TestClass.const_defined?(:TestsTestClassDecorator))
+    decorate(TestClass, with: 'more_tests') {}
+    assert(TestClass.const_defined?(:MoreTestsDecorateTestTestClassDecorator))
   end
 end
