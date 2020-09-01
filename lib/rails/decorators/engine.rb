33969c57c2ec8f9445159df8a4c9e9bb6dbb1e7d
Ben Crouse <bcrouse@workarea.com>
2020-09-01 12:25:01 -0400

    Raise if Rails isn't using Zeitwerk

--- lib/rails/decorators/engine.rb
+++ lib/rails/decorators/engine.rb
@@ -1,10 +1,16 @@
 module Rails
   module Decorators
+    class MissingZeitwerkError < StandardError; end
+
     class Engine < ::Rails::Engine
       isolate_namespace Rails::Decorators
 
-      config.before_initialize do |app|
-        app.config.autoloader = :zeitwerk
+      config.after_initialize do |app|
+        unless Rails.configuration.autoloader == :zeitwerk
+          raise MissingZeitwerkError, <<-eos.strip_heredoc
+            rails-decorators requires using the Zeitwerk code loader. You can set this in config/application.rb by doing `config.autoloader = :zeitwerk`
+          eos
+        end
       end
     end
   end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -13,5 +13,7 @@ module Dummy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.load_defaults "6.0"
   end
 end
